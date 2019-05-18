942ec5e41bf8d6b558fd39645ab7341b21528020
Alejandro Tidele <aleetidele@hotmail.com>
2019-05-17 23:43:07 -0300

    Agregado de archivos faltantes.

---
+++ TP_WEB_Tidele_Dangelo/Index.aspx.designer.cs
@@ -0,0 +1,15 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace TP_WEB_Tidele_Dangelo {
+    
+    
+    public partial class Index {
+    }
+}
