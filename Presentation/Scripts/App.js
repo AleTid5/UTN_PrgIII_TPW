a0eef31942b7417275ea759e31a684b6478b80e1
AleTid5 <aleetidele@gmail.com>
2019-05-22 21:13:07 -0300

    CC

--- Presentation/Scripts/App.js
+++ Presentation/Scripts/App.js
@@ -1,1 +1,25 @@
-﻿
+﻿function getURL(url, data) {
+    let response = null;
+
+    $.ajax({
+        url: url + O2Q(data),
+        method: 'GET',
+        dataType: 'json',
+        async: false,
+        success: (JsonResponse) => {
+            response = JSON.parse(JsonResponse)
+        }
+    })
+
+    return response;
+}
+
+function O2Q(object) {
+    let query = '?';
+
+    Object.keys(object).map(key => {
+        query += key + "=" + object[key] + "&";
+    });
+
+    return query;
+}
--- Presentation/Scripts/material-bootstrap-wizard.js
+++ Presentation/Scripts/material-bootstrap-wizard.js
@@ -19,32 +19,6 @@ var searchVisible = 0;
 var transparent = true;
 var mobile_device = false;
 
-function getURL(url, data) {
-    let response = null;
-
-    $.ajax({
-        url: url + O2Q(data),
-        method: 'GET',
-        dataType: 'json',
-        async: false,
-        success: (JsonResponse) => {
-            response = JSON.parse(JsonResponse)
-        }
-    })
-
-    return response;
-}
-
-function O2Q(object) {
-    let query = '?';
-
-    Object.keys(object).map(key => {
-        query += key + "=" + object[key] + "&";
-    });
-
-    return query;
-}
-
 $(document).ready(function () {
     $.material.init();
 
